159f8907adc018b43a94c1a51871189957cf438a
Ondřej Duchoň <duchon.ondrej@gmail.com>
2015-03-08 22:51:24 +0100

    JS Split

---
+++ Scripts/install.js
@@ -0,0 +1,11 @@
+function dalsiKrok(elementID1, elementID2)
+{
+	document.getElementById(elementID2).style.display = 'block';
+	document.getElementById(elementID1).style.display = 'none';			
+}
+
+function predchoziKrok(elementID1, elementID2)
+{
+	document.getElementById(elementID2).style.display = 'block';
+	document.getElementById(elementID1).style.display = 'none';			
+}

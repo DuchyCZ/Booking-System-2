1bd1c497f5e90a246dc38e93cabb410376afa75c
Ondřej Duchoň <duchon.ondrej@gmail.com>
2015-03-09 13:28:37 +0100

    More intall stuff

--- Scripts/install.js
+++ Scripts/install.js
@@ -1,19 +1,43 @@
-function dalsiKrok(elementID1, elementID2)
+$(document).ready(function(){
+ 
+	/*$("#kroktext1").toggle(2000);
+	$("#kroktext1").toggle(2000);*/
+ 
+});
+
+function dalsiKrok()
 {
-	document.getElementById(elementID2).style.display = 'block';
-	document.getElementById(elementID1).style.display = 'none';	
+	document.getElementById("kroktext2").style.display = 'block';
+	document.getElementById("kroktext1").style.display = 'none';	
 }
 
-function dalsiKrokKontrola1(elementID1, elementID2, elementID3, elementID4, elementID5)
+function dalsiKrokKontrola1()
 {
-	if (document.getElementById(elementID3).value == "" || document.getElementById(elementID4).value == "" || document.getElementById(elementID5).value == "")
+	if (document.getElementById("kurty").value == "" || document.getElementById("kurty").value == 0)
 	{	
-		alert("Všechny hodnoty musí být vyplněny!");
+		document.getElementById("kurty").style.borderColor='#FF0000';		
+		$("#kurty").fadeOut(50).fadeIn(200);
 	}
-	else
+	else document.getElementById("kurty").style.borderColor='#04B404';
+	
+	if (document.getElementById("hodiny").value == "")
 	{
-		document.getElementById(elementID2).style.display = 'block';
-		document.getElementById(elementID1).style.display = 'none';	
+		document.getElementById("hodiny").style.borderColor='#FF0000';		
+		$("#hodiny").fadeOut(50).fadeIn(200);
+	}
+	else document.getElementById("hodiny").style.borderColor='#04B404';
+	
+	if (document.getElementById("hodiny2").value == "")
+	{
+		document.getElementById("hodiny2").style.borderColor='#FF0000';	
+		$("#hodiny2").fadeOut(50).fadeIn(200);
+	}
+	else document.getElementById("hodiny2").style.borderColor='#04B404';
+	
+	if (document.getElementById("kurty").value != "" && document.getElementById("hodiny").value != "" && document.getElementById("hodiny2").value != "")
+	{
+		document.getElementById("kroktext3").style.display = 'block';
+		document.getElementById("kroktext2").style.display = 'none';	
 	}
 }
 

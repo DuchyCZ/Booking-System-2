eadb80e3fe162dab237eed640c4edb8cace66c7c
Ondřej Duchoň <duchon.ondrej@gmail.com>
2015-03-12 16:37:12 +0100

    More install stuff 4
    More tooltips stuff

--- Scripts/install.js
+++ Scripts/install.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
 				$('.tkurtyo').remove();
 				$('.tkurty').remove();
 				var p = $("#kurty").offset();		
-				neerror("OK", p.top - $("#kurty").height() / 2, p.left + $("#kurty").width(), "tkurtyo");	
+				tooltipNeerror("OK", p.top - $("#kurty").height() / 2, p.left + $("#kurty").width(), "tkurtyo");	
 				document.getElementById("kurty").style.borderColor='#009900';
 			}	
 			else
@@ -17,13 +17,34 @@ $(document).ready(function(){
 				$('.tkurtyo').remove();
 				$('.tkurty').remove();
 				var p = $("#kurty").offset();		
-				error("Parametr musí být vyplněn!", p.top - $("#kurty").height() / 2, p.left + $("#kurty").width(), "tkurty");
+				tooltipError("Parametr musí být vyplněn!", p.top - $("#kurty").height() / 2, p.left + $("#kurty").width(), "tkurty");
 				document.getElementById("kurty").style.borderColor='#FF0000';
 			}
 	  })
+	  
+	$("#hodiny2")
+		.focusout(function() {
+			if ($("#hodiny").val() != "" && $("#hodiny2").val() != "")
+			{
+				$('.hodiny2o').remove();
+				$('.hodiny2').remove();
+				var p = $("#hodiny2").offset();		
+				tooltipNeerror("OK", p.top - $("#hodiny2").height() / 2, p.left + $("#hodiny2").width(), "hodiny2o");	
+				document.getElementById("hodiny2").style.borderColor='#009900';
+				document.getElementById("hodiny").style.borderColor='#009900';
+			}	
+			/*else
+			{
+				$('.tkurtyo').remove();
+				$('.tkurty').remove();
+				var p = $("#kurty").offset();		
+				tooltipError("Parametr musí být vyplněn!", p.top - $("#kurty").height() / 2, p.left + $("#kurty").width(), "tkurty");
+				document.getElementById("kurty").style.borderColor='#FF0000';
+			}*/
+	  })
 });
 
-function error(text, px, py, id)
+function tooltipError(text, px, py, id)
 {
 	$('<p class="'+ id +'"></p>')
 	.text(text)
@@ -34,8 +55,9 @@ function error(text, px, py, id)
 	.fadeOut('slow');
 }
 
-function neerror(text, px, py, id)
+function tooltipNeerror(text, px, py, id)
 {
+	tooltips.push(id);
 	$('<p class="'+ id +'"></p>')
 	.text(text)
 	.appendTo('body')
@@ -50,14 +72,14 @@ function dalsiKrok()
 }
 
 function dalsiKrokKontrola1()
-{
+{	
 	if (document.getElementById("kurty").value == "" || document.getElementById("kurty").value == 0)
 	{	
 		document.getElementById("kurty").style.borderColor='#FF0000';
 		
 		$('.tkurty').remove();
 		var p = $("#kurty").offset();		
-		error("Parametr musí být vyplněn!", p.top - $("#kurty").height() / 2, p.left + $("#kurty").width(), "tkurty");		
+		tooltipError("Parametr musí být vyplněn!", p.top - $("#kurty").height() / 2, p.left + $("#kurty").width(), "tkurty");		
 	}
 	
 	if (document.getElementById("hodiny").value == "") document.getElementById("hodiny").style.borderColor='#FF0000';
@@ -68,11 +90,12 @@ function dalsiKrokKontrola1()
 		
 		$('.thodiny2').remove();
 		var p3 = $("#hodiny2").offset();		
-		error("Parametr musí být vyplněn!", p3.top - $("#hodiny2").height() / 2, p3.left + $("#hodiny2").width(), "thodiny2");	
+		tooltipError("Parametr musí být vyplněn!", p3.top - $("#hodiny2").height() / 2, p3.left + $("#hodiny2").width(), "thodiny2");	
 	}
 	
 	if (document.getElementById("kurty").value != "" && document.getElementById("hodiny").value != "" && document.getElementById("hodiny2").value != "")
 	{
+		$('.tkurtyo').remove();
 		document.getElementById("kroktext3").style.display = 'block';
 		document.getElementById("kroktext2").style.display = 'none';	
 	}
@@ -81,5 +104,12 @@ function dalsiKrokKontrola1()
 function predchoziKrok(elementID1, elementID2)
 {
 	document.getElementById(elementID2).style.display = 'block';
-	document.getElementById(elementID1).style.display = 'none';			
+	document.getElementById(elementID1).style.display = 'none';	
+	$('.tkurtyo').remove();
+	
+	if (elementID2 != "kroktext1")
+	{
+		var p = $("#kurty").offset();		
+		tooltipNeerror("OK", p.top - $("#kurty").height() / 2, p.left + $("#kurty").width(), "tkurtyo");		
+	}
 }
